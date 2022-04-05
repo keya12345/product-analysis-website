a415763c4cc7b1ce93294be92737a86dc5e0f24d
keya12345 <0319452930@student.wub.edu.bd>
2022-04-05 14:43:05 +0600

    home complete

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./Header.css";
 const Header = () => {
   return (
     <div>
-      <nav className="">
-        <Link to="/home">Home</Link>
-        <Link to="/review">Review</Link>
-        <Link to="/blogs">Blogs</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/about">About</Link>
+      <nav className="navigation">
+        <div>
+          <h1>FASHION HOUSE</h1>
+        </div>
+        <div className=" mr-3 link">
+          <Link to="/home">Home</Link>
+          <Link to="/review">Review</Link>
+          <Link to="/blogs">Blogs</Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/about">About</Link>
+        </div>
       </nav>
     </div>
   );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,26 @@
 import React from "react";
-
+import "./Home.css";
 const Home = () => {
   return (
-    <div>
-      <h1>home</h1>
+    <div className="grid grid-cols-2 gap-4">
+      <div>
+        <h1 className="text-6xl font-semibold text-purple-800 mt-8 mb-8">
+          MY FASHION HOUSE
+        </h1>
+        <p className="text-2xl font-semibold mx-3">
+          It is also defined as a fashionable clothes company: a business that
+          designs, makes, and sells fashionable clothes that are typically
+          associated with an important designer, or a company that designs news
+          styles of expensive clothes.
+        </p>
+        <button className="rounded-full bg-teal-400 mx-4 h-8 w-16  my-4 mx-4">
+          DEMO
+        </button>
+      </div>
+
+      <div className="image">
+        <img src="download.jpg" alt="" />
+      </div>
     </div>
   );
 };
